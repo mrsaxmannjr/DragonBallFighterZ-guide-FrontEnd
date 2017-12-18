8d1f49421c8449d7e94ae93a95021a8a4337f68c
mrsaxmannjr <mrsaxmannjr@gmail.com>
2017-12-18 11:34:59 -0700

    cya

--- src/index.js
+++ src/index.js
@@ -1,22 +1,59 @@
 const baseURL = "https://radiant-coast-65532.herokuapp.com/";
+var glbresponse = [];
 
 fetch(baseURL)
   .then(response => response.json())
   .then(response => {
-    var charPallete = document.querySelector(".characterPallete");
-
+    glbresponse.push(response);
+    var charPalette = document.querySelector(".characterPalette");
+    console.log("global response: ", glbresponse);
 
     for (var i = 0; i < response.length; i++) {
-      var charDiv = document.createElement("div");
-      charDiv.textContent = response[i].name;
-      charPallete.appendChild(charDiv);
+      var charAtag = document.createElement("a");
+      charAtag.textContent = response[i].name;
+      charAtag.classList.add(
+        "waves-effect",
+        "waves-light",
+        "btn",
+        "modal-trigger"
+      );
+      charAtag.href = "#modal1";
+
+      charAtag.addEventListener("click", event => {
+        for (var j = 0; j < glbresponse[0].length; j++) {
+          if (glbresponse[0][j].name === event.target.textContent) {
+            document.querySelector("h4").textContent = glbresponse[0][j].name;
+            document.querySelector("h5").textContent = "Race: " + glbresponse[0][j].race;
+            document.querySelector(".bio").textContent = glbresponse[0][j].bio;
+            document.querySelector(".power").textContent =
+              "Power: " + glbresponse[0][j].Power;
+            document.querySelector(".speed").textContent =
+              "Speed: " + glbresponse[0][j].Speed;
+            document.querySelector(".technique").textContent =
+              "Technique: " + glbresponse[0][j].Technique;
+            document.querySelector(".reach").textContent =
+              "Reach: " + glbresponse[0][j].Reach;
+            document.querySelector(".energy").textContent =
+              "Energy: " + glbresponse[0][j].Energy;
+            document.querySelector(".easeOfUse").textContent =
+              "Ease of use: " + glbresponse[0][j]["Ease of use"];
+          }
+        }
+      });
+
+      charPalette.appendChild(charAtag);
     }
   })
   .catch(err => console.log(err));
 
-fetch("https://radiant-coast-65532.herokuapp.com/",  {
+document.querySelector(#addToTeam).addEventListener("click", () => {
+  
+})
+
+
+fetch("https://radiant-coast-65532.herokuapp.com/", {
   method: "post",
-  body: JSON.stringify({greeting: "hello"}),
+  body: JSON.stringify({ greeting: "hello" }),
   headers: new Headers({
     "Content-Type": "application/json"
   })
@@ -24,3 +61,8 @@ fetch("https://radiant-coast-65532.herokuapp.com/",  {
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.log(err));
+
+$(document).ready(function() {
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $(".modal").modal();
+});
